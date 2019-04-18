51fdf5077c8c57c5cb3059c6ae7a56acfad1b36c
Shehanka <hcsperera@outlook.com>
2019-04-19 00:50:16 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bookish_fortnight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
